c6bbb897f8b63ef7afd3e88390eca2363bfe4b7b
Joel Gt <0332201030@unjfsc.edu.pe>
2024-08-15 20:54:23 -0500

    Insertando La configuracion de la Bd de mongo DB

---
+++ API/src/config.ts
@@ -0,0 +1,6 @@
+export default{
+    MONGO_DATABASE : 'market-pos',
+    MONGO_USER :"ad,in",
+    MONGO_PASSWORD:"admin",
+    MONGO_HOST : "localhost"
+}
--- API/src/index.ts
+++ API/src/index.ts
@@ -1,7 +1,19 @@
-import express from 'express'    
+import { json } from "stream/consumers";
+import  app  from "./app";
+import './database'
 
-const app = express();
+app.get('/',(req,res)=>{
+    res.send('Bienvenido')
+})
 
+app.get('/productos',(req,res)=>{
+    res.send('Todos los productos')
+})
+
+app.get('/ListProducto',(req,res)=>{
+    res.json({Message : "Hola Bienvenido"})
+})
+ 
 app.listen(3000 , ()=>{
     console.log("Server Activo 3000")
 })
